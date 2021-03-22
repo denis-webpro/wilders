e8637df3f042777610bff9990c8b76a9ffe75ffb
denis-webpro <denis.webpro@gmail.com>
2021-03-22 16:23:52 +0100

    Envoi des fichiers

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require ('chalk');
+
+console.log(chalk.yellow('Cédric'));
+console.log(chalk.red('Denis'));
+console.log(chalk.blue('Pascal'));
+console.log(chalk.green('Thomas'));
+
+
